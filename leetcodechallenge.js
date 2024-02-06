f5cf584fb8cb7ec0b149ca6a9a89c8b296a1b4a5
mukesh <kumarmukesh789123@gmail.com>
2024-02-06 11:45:28 +0500

    first commit

---
+++ leetcodechallenge.js
@@ -0,0 +1,112 @@
+// function expect(val){
+//     return {
+//         toBe: function(expected){
+//             if ( val !== expected ){
+//                 throw new Error("Not Equal")
+//             }
+//         return true
+//         },
+
+//         notToBe: function(unexpected) {
+//             if ( val === unexpected){
+//                 throw new Error('Equal')
+//             }
+//         return true
+//         }
+
+
+//     };
+// }
+
+// try{
+//     // expect(5).toBe(5)
+//     // expect(5).notToBe(5)
+//     expect(5).toBe('5')
+// }
+// catch(error){
+//     console.log(error.message);
+// }
+
+// let a=5
+// let b=5
+// a === b
+// a !== b
+// // values data types
+// // console.log(a===b)
+
+// console.log(0.1+0.2===0.3);
+
+
+
+// var x = 5;
+// console.log( x++ + ++x );
+
+
+// function createCounter(init) {
+//     let count=init;
+//     return {
+//         increment: function(){
+//             count++;
+//             return count;
+//         },
+
+//         decrement: function(){
+//             count--;
+//             return count;
+
+//         },
+
+//         reset: function(){
+//             count=init;
+//             return count
+//         }
+//     }
+
+// }
+
+// const value=createCounter(5)
+
+// console.log(value.increment())
+// console.log(value.decrement())
+// console.log(value.reset())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
